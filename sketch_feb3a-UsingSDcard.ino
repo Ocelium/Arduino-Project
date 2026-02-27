95e6a1232fd383ec5a9917abb646246dde6797cd
rayenbenk7 <rayenbenk7@gmail.com>
2026-02-27 10:14:26 -0500

    Joystic to Data in SDcard Module - Arduino Code

---
+++ sketch_feb3a-UsingSDcard.ino
@@ -0,0 +1,78 @@
+#include <SPI.h>
+#include <SD.h>
+
+const int chipSelect = 10;
+File myFile;
+
+#define VRX_PIN  A1
+#define VRY_PIN  A0
+
+int xValue = 0;
+int yValue = 0;
+
+void setup() {
+
+  Serial.begin(9600);
+  while (!Serial);
+
+  pinMode(chipSelect, OUTPUT);
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(chipSelect)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("1. is a card inserted?");
+    Serial.println("2. is your wiring correct?");
+    Serial.println("3. did you change the chipSelect pin to match your shield or module?");
+    Serial.println("Note: press reset button on the board and reopen this Serial Monitor after fixing your issue!");
+    while (true);
+  }
+
+  Serial.println("initialization done.");
+
+ 
+}
+
+void loop() {
+
+
+  myFile = SD.open("test.txt", FILE_WRITE);
+
+    // if the file opened okay, write to it:
+  if (myFile) {
+    //Joystick stuff here
+    xValue = analogRead(VRX_PIN);
+    yValue = analogRead(VRY_PIN);
+
+    myFile.print("X:");
+    myFile.print(xValue);
+    myFile.print(" Y:");
+    myFile.println(yValue);
+    
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
+
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+
+  //Read it now
+  myFile = SD.open("test.txt");
+  if (myFile) {
+    Serial.println("test.txt:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+  delay(200);
+}
+
