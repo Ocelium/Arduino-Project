c7b5fcbbd9ef74ba186cb5a5c7b56df51d3d7c7a
rayenbenk7 <rayenbenk7@gmail.com>
2026-04-18 21:02:55 -0400

    Simple picture taking to SD w/ ESP32-cam
    The code works by taking a certain amount of pictures (100 default), at 5fps (so 20s worth of videos). The pictures start being taken when a message is sent in the serial monitor, and begins with a flash when such is done.

---
+++ esp32cam_photo.ino
@@ -0,0 +1,143 @@
+#include "esp_camera.h"
+#include "SD_MMC.h"
+#include "FS.h"
+
+
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+#define LED_PIN 4
+
+bool recording = false;
+
+void flashBlink() {
+  // ~50% brightness flash (PWM)
+  ledcAttach(LED_PIN, 5000, 8);
+  ledcWrite(LED_PIN, 128);
+  delay(200);
+  ledcWrite(LED_PIN, 0);
+}
+
+void captureFrames() {
+  int totalFrames = 100;
+  int delayPerFrame = 200;
+
+  for (int i = 0; i < totalFrames; i++) {
+
+    camera_fb_t *fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Capture failed");
+      return;
+    }
+
+    String path = "/frame_" + String(i) + ".jpg";
+
+    File file = SD_MMC.open(path, FILE_WRITE);
+    if (file) {
+      file.write(fb->buf, fb->len);
+      file.close();
+      Serial.printf("Saved: %s\n", path.c_str());
+    } else {
+      Serial.println("File open failed");
+    }
+
+    esp_camera_fb_return(fb);
+    delay(delayPerFrame);
+  }
+
+  Serial.println("Done capturing.");
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // Camera config
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer   = LEDC_TIMER_0;
+  config.pin_d0       = Y2_GPIO_NUM;
+  config.pin_d1       = Y3_GPIO_NUM;
+  config.pin_d2       = Y4_GPIO_NUM;
+  config.pin_d3       = Y5_GPIO_NUM;
+  config.pin_d4       = Y6_GPIO_NUM;
+  config.pin_d5       = Y7_GPIO_NUM;
+  config.pin_d6       = Y8_GPIO_NUM;
+  config.pin_d7       = Y9_GPIO_NUM;
+  config.pin_xclk     = XCLK_GPIO_NUM;
+  config.pin_pclk     = PCLK_GPIO_NUM;
+  config.pin_vsync    = VSYNC_GPIO_NUM;
+  config.pin_href     = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn     = PWDN_GPIO_NUM;
+  config.pin_reset    = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format  = PIXFORMAT_JPEG;
+
+  if (psramFound()) {
+    config.frame_size   = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count     = 2;
+  } else {
+    config.frame_size   = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count     = 1;
+  }
+
+  if (esp_camera_init(&config) != ESP_OK) {
+    Serial.println("Camera init failed");
+    return;
+  }
+
+  if (!SD_MMC.begin("/sdcard", true)) {
+    Serial.println("SD mount failed");
+    return;
+  }
+
+  Serial.println("Ready. Send any message in Serial to start.");
+}
+
+void loop() {
+
+  if (Serial.available()) {
+
+    String msg = Serial.readStringUntil('\n');
+    msg.trim();
+
+    if (msg.length() > 0 && !recording) {
+
+      recording = true;
+
+      Serial.println("Trigger received");
+
+      // blink flash
+      flashBlink();
+
+      // wait 0.5s before starting capture
+      delay(500);
+
+      captureFrames();
+
+      recording = false;
+
+      // blink flash
+      flashBlink();
+
+      Serial.println("Waiting for next trigger...");
+    }
+  }
+}
